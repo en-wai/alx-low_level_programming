b41180bf4e19df47ea25262e4332c60a881d96fe
en-wai <nyasare@hotmail.com>
2022-10-17 14:44:11 -0700

    create compile file

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (0n < 0)
+	{
+		printf(n + "is negative")
+	}
+	if (n > 0)
+	{
+		printf(n + "is positive")
+	}
+	return (0);
+}
