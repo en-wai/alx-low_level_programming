4d319b193957d19d5abd6d78058a8d8e6c70a4df
en-wai <nyasare@hotmail.com>
2022-10-18 16:50:15 -0700

    Create func to print lower_case alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/*
+ * File: 1-alphabet.c
+ * Auth: En-wai Asare
+ */
+
+
+/**
+ * main - Prints out _putschar.
+ *
+ * Return: Always void.
+ */
+void print_alphabet(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		_putchar(letter <= 'z')
+		letter++;
+	}
+	_putchar('\n');
+}
