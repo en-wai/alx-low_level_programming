3c529c6a2b41a08d57cb970094b029335724e5f1
en-wai <nyasare@hotmail.com>
2022-10-18 08:37:15 -0700

    Create a script to print lowercase alphabets

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+/*
+ * File: 4-print_alphabt.c
+ * Auth: En-wai Asare
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Print the alphabet in lowercase.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if(letter != 'e' && letter != 'q')
+			putchar(letter);
+	}
+	
+	putchar('\n');
+
+	return (0);
+}	
