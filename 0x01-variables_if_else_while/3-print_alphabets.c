421cdeaea947603ac32065c2b40420c41d961ca2
en-wai <nyasare@hotmail.com>
2022-10-18 08:18:47 -0700

    Create a scriipt to print both lower and upper case

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -17,7 +17,7 @@ int main(void)
 	for (letter = 'a'; letter <= 'z'; letter++)
 		putchar(letter);
 	for (letter = 'A'; letter <= 'z'; letter++)
-
+	      	putchar(letter);
 	putchar('\n');
 
 	return (0);
