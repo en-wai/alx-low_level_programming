97ba3dd6ec8cad61992de5aaa1c48b4d267d3bad
en-wai <nyasare@hotmail.com>
2022-10-18 11:00:08 -0700

    Create a script to print combination of numbers and characters.

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -14,10 +14,10 @@ int main(void)
 {
 	int num;
 
-	for (num = 0; num < 9; num++)
+	for (num = 0; num < 10; num++)
 	{
 		putchar((num % 10) + '0');
-		if (num == 10)
+		if (num == 9)
 			continue;
 		putchar(',');
 		putchar(' ');
