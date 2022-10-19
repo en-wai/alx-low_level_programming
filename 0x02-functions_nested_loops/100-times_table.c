82f7792163537506a83e3cd17358789a4cff8a96
en-wai <nyasare@hotmail.com>
2022-10-19 00:40:12 -0700

    Create a func to print 100 Time Table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * print_times_table - Prints a multiplication table up to param
+ * times_table - Prints a multiplication table up to param
  * @n: The number to be treated
  *
  * Return: Number matrix
  */
-void print_times_table(int n)
+void times_table(int n)
 {
 	int x, y, z;
 
