b6c18a5e1ac92c2073e16913b610ffb1140d71df
en-wai <nyasare@hotmail.com>
2022-10-18 17:27:21 -0700

    Create func to check if alphabet is lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/*
+ * File: 2-print_alphabet_x10.c
+ * Auth: En-wai Asare
+ */
+
+
+/**
+ * _islower - Main entry point.
+ * Description: Verifies lowercase char
+ *@c: received int value
+ * Return: 1 if true and 0 if false.
+ */
+int _islower(int c)
+{
+	int i = 'a';
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		/* refererence int c*/
+	if (c == 1)
+	{
+	return (1);
+	}
+	}
+	return (0);
+}
+
