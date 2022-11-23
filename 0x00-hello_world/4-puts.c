48b48c8121425834389e7b8d7248f14688b5e918
en-wai <nyasare@hotmail.com>
2022-11-23 12:25:00 +0200

    Add header descritption and remove return value

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,16 @@
 /**
  * Filename: 4-puts.c
  * Author: En-wai Asare
+ *
+ * Description: Uses the puts() func to print string
  */
 
 #include <stdio.h>
 
 /**
  * main - Entry point
- * Return: Always 0 (Success)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
 }
