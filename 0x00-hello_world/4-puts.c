d62161d6c1bf64444cd5a11f0afcb3dc292c0703
en-wai <nyasare@hotmail.com>
2022-11-23 12:34:33 +0200

    Correct betty lint errors for description and return value in header

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -9,6 +9,10 @@
 
 /**
  * main - Entry point
+ *
+ * Description: Calls puts and recieves value returned by puts after its
+ * execution.
+ * Return: Always return 0 to indicate success
  */
 int main(void)
 {
