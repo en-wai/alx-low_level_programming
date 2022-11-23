632b9f08274dec3501fdaebea51924024ec5e3d7
en-wai <nyasare@hotmail.com>
2022-11-23 12:28:43 +0200

    Correct betty lint errors

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -10,8 +10,8 @@
 /**
  * main - Entry point
  */
-int main(void)
+int main (void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	puts ("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
