502ba355a089d256e56b2b47fe0dafabd1a1911c
en-wai <nyasare@hotmail.com>
2022-12-24 19:14:16 +0200

    Add more prototypes

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -3,20 +3,20 @@
  * Author: En-wai Asare
  *
  */
-
 #include "main.h"
 
 /**
- * main - tests function that prints whether an integer is positive or negative
+ * main - Test function for positive or negative
  * Return: 0
  */
 
 int main(void)
 {
-	int i;
+ int i;
 
-	i = 98;
-	positive_or_negative(i);
+ i = 0;
+ positive_or_negative(i);
 
-	return (0);
+return (0);
 }
+
