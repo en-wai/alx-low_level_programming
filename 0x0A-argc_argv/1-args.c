1e0a9e620b0381fe5b508433f28d1561f893c1ad
en-wai <nyasare@hotmail.com>
2023-02-06 01:20:50 +0200

    Remove blank spaces in problem 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -4,7 +4,7 @@
  * main - print the number of arguments passed to the program
  * @argc: number of arguments
  * @argv: array of arguments
- * 
+ *
  * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
