331b38e8d7926fc99a7581f3afc86f25967ff233
en-wai <nyasare@hotmail.com>
2023-02-06 01:06:30 +0200

    Remove blank spaces

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,7 +4,7 @@
  * main - prints the name of the program
  * @argc: number of arguments
  * @argv: array of arguments
- * 
+ *
  * Return: Always 0 (Success)
  **/
 int main(int argc __attribute__((unused)), char *argv[])
