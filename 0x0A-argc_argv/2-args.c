8515c3fd0f33c025dc86d46d3873e8d331b87913
en-wai <nyasare@hotmail.com>
2023-02-06 01:34:00 +0200

    Remove blank spaces in problem 2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * main - prints all arguments it receives
+ * main - prints all arguments it receives.
  * @argc: number of arguments
  * @argv: array of arguments
  *
