3f63e6a29d9562b5df77c6b1269376e2f109a53f
en-wai <nyasare@hotmail.com>
2023-02-06 00:59:51 +0200

    Remove blank spaces

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include "main.h"
-				
 /**
  * main - prints the name of the program
  * @argc: number of arguments
@@ -8,9 +7,8 @@
  *
  * Return: Always 0 (Success)		
  */
-int main(int argc __attribute__((unused)), char *argv[])		
-{	
+int main(int argc __attribute__((unused)), char *argv[])
+{
 	printf("%s\n", *argv);
-
-	return (0);		
+	return (0);
 }
