75966f8ea12c61551f7a3121ce45d10febe81878
en-wai <nyasare@hotmail.com>
2023-02-06 01:15:06 +0200

    Remove blank spaces in problem 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,9 +6,9 @@
  * @argv: array of arguments
  * 
  * Return: Always 0 (Success)
- * */
+ */
 int main(int argc, char *argv[])
-{
 (void) argv; /*Ignore argv*/
 	printf("%d\n", argc - 1);
+
 	return (0);}
