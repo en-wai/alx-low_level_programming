c82e3fd9633eb4fdc3278e45a68f8f37c7607015
en-wai <nyasare@hotmail.com>
2023-02-06 03:12:55 +0200

    Remove blank spaces in problem 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -3,7 +3,7 @@
 /**
  * _atoi - converts a string to an integer
  * @s: string to be converted
- * 
+ *
  * Return: the int converted from the string
  */
 int _atoi(char *s)
@@ -38,27 +38,23 @@ int _atoi(char *s)
 		return (0);
 	return (n);
 }
-				
 /**
  * main - multiplies two numbers
  * @argc: number of argument
  * @argv: array of argument		
- *
  * Return: 0 (Success), 1 (Error)		
  */
-int main(int argc, char *argv[])		
+int main(int argc, char *argv[])
 {
 	int result, num1, num2;
 
-	if (argc < 3 || argc > 3;	
+	if (argc < 3 || argc > 3;
 	{
-		printf("Error\n");
-		return (1);	
+		iprintf("Error\n");
+		return (1);
 	}
 	num1 = _atoi(argv[1]);
 	num2 = _atoi(argv[2]);
-	result = num1 * num2;		
+	result = num1 * num2;
 	printf("%d\n", result);
-
 	return (0);
-}
