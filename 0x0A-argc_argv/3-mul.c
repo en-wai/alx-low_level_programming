f14bb4a4d23eb6dafd69283c6bb47b3368261742
en-wai <nyasare@hotmail.com>
2023-02-06 03:06:49 +0200

    Remove blank spaces in problem 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,32 +1,26 @@
 #include <stdio.h>
-#include "main.h"		
+#include "main.h"
 /**
  * _atoi - converts a string to an integer
- * @s: string to be converted		
- *
- * Return: the int converted from the string		
+ * @s: string to be converted
+ * 
+ * Return: the int converted from the string
  */
 int _atoi(char *s)
 {
 	int i, d, n, len, f, digit;
-			
-	i = 0;	
-	d = 0;	
+	i = 0;
+	d = 0;
 	n = 0;
-	len = 0;	
+	len = 0;
 	f = 0;
 	digit = 0;
-		
-	while (s[len] != '\0')		
+	while (s[len] != '\0')
 		len++;
-		
-
-	while (i < len && f == 0)	
+	while (i < len && f == 0)
 	{
 		if (s[i] == '-')
 			++d;
-		
-		
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			digit = s[i] - '0';
@@ -38,13 +32,10 @@ int _atoi(char *s)
 				break;
 			f = 0;
 		}
-		i++;	
+		i++;
 	}
-		
 	if (f == 0)
-
 		return (0);
-		
 	return (n);
 }
 				
